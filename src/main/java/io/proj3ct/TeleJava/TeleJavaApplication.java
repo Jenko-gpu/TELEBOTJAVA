3925d28928cf11a15bd3cb89b5b777c08abb59c2
Jenko-gpu <74445915+Jenko-gpu@users.noreply.github.com>
2023-10-02 20:38:26 +0500

    1

---
+++ src/main/java/io/proj3ct/TeleJava/TeleJavaApplication.java
@@ -0,0 +1,13 @@
+package io.proj3ct.TeleJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeleJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeleJavaApplication.class, args);
+	}
+
+}
